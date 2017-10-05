d3e45ee4d233935ca6d5878af35839c158d2f208
chiefautoparts <chiefautoparts@outlook.com>
2017-10-05 11:43:22 -0700

    super sweet about us page styling

--- oliviaFlowers/urls.py
+++ oliviaFlowers/urls.py
@@ -18,5 +18,6 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^', include('apps.home.urls', namespace='home'))
+    url(r'^', include('apps.home.urls', namespace='home')),
+    url(r'^about', include('apps.about.urls', namespace='about'))
 ]
