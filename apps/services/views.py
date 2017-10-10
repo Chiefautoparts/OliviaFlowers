511d7353549ee91537e4d143db501d482463d2c8
chiefautoparts <chiefautoparts@outlook.com>
2017-10-09 22:30:58 -0700

    did some stuff but dont remember what i did

--- apps/services/views.py
+++ apps/services/views.py
@@ -1,9 +1,18 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
 def index(request):
 	print '**services**' * 200
 	return render(request, 'services/services.html')
+
+def events(request):
+	print '**events**' * 250
+
+def designs(request):
+	print '**designs**' * 250
+
+def seasonal(request):
+	print '**seasonal**' * 250
